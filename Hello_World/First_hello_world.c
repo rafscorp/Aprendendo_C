f18a22484fffb6f40282e7bbd0a3c05e4b52f8a9
rafscorp <metsaccout@gmail.com>
2025-12-13 21:03:27 -0300

    Adiciona exemplos Hello World em C

---
+++ Hello_World/First_hello_world.c
@@ -0,0 +1,5 @@
+#include <stdio.h>  // Biblioteca das Funcões principais de entrada e saída
+
+int main() {  //O corpo pode exitir somente um main ele é o corpo do programa ele é uma função que baseada no codigo pode retornar 1 ou 0 | verdadeiro ou falso
+  printf("Hello, world | Olá mundo");  //função da biblioteca <stdio.h> "input" e "output"
+}
