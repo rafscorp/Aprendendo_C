01ce4ac022c0392411a145a209e6d44ba22b602f
rafscorp <metsaccout@gmail.com>
2025-12-13 21:51:18 -0300

    Meu primeiro hello world em C

--- Hello_World/First_hello_world.c
+++ Hello_World/First_hello_world.c
@@ -1,4 +1,4 @@
-#include <stdio.h>  // Biblioteca das Funcões principais de entrada e saída
+#include <stdio.h>  // Biblioteca das Funcões principais de entrada e saída IO
 
 int main() {  //O corpo pode exitir somente um main ele é o corpo do programa ele é uma função que baseada no codigo pode retornar 1 ou 0 | verdadeiro ou falso
   printf("Hello, world | Olá mundo");  //função da biblioteca <stdio.h> "input" e "output"
