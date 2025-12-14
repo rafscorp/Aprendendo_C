3d2608c5f4cdd8befec103d092a984308dc99150
rafscorp <metsaccout@gmail.com>
2025-12-13 21:03:27 -0300

    Adiciona exemplos de estruturas condicionais (if/else) em C

---
+++ Condicoes_If_Else/Par_ou_Impar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int Num1;
+
+    printf("Digite um Número para a verificação: ");
+    scanf("%d", &Num1);
+
+    if (Num1 % 2 == 0) {
+        printf("O seu Número ( %d ) é Par \n", Num1);
+    } else {
+        printf("Seu Número ( %d ) é ímpar \n", Num1);
+    }
+}
---
+++ Condicoes_If_Else/Primeiro_if_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int Num1;
+
+    printf("---Programa | Verdadeiro ou Falso | < 5 False | > 5 True\n Informe um Número: ");
+    scanf("%d", &Num1);
+
+    if (Num1 <= 5) {
+        printf("Hmm..Número Falso");
+    } else {
+        printf("Aee..Número Verdadeiro");
+    }
+
+}
